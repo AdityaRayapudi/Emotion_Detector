ea43c9b3dba1673e1e623c74d39a0804ba0bec51
Aditya Rayapudi <arayapudi2006@gmail.com>
2025-04-06 16:49:45 -0400

    Reset Port to Default

--- app.py
+++ app.py
@@ -159,5 +159,5 @@ if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    socketio.run(app, debug=False, host='0.0.0.0', port=8000)
+    socketio.run(app, debug=False)
     # app.run(debug=False)
